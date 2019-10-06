4b8dc8f3e0dd8d468ca026c7aed036e1daad7e19
Uli Schlachter <psychon@znc.in>
2019-10-06 19:17:15 +0200

    More improvements to the code generator
    This started with adding support for requests with replies, but then
    ended up also having some smaller fixes. For examples, all the necessary
    "pub"s should be there now.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- rs_code_generator.py
+++ rs_code_generator.py
@@ -37,7 +37,7 @@ def _out_indent_decr():
     _indent_level -= 1
 
 def _name(name):
-    assert len(name) == 2
+    assert len(name) == 2, name
     return name[1]
 
 rust_type_mapping = {
@@ -56,6 +56,13 @@ def _to_rust_type(name):
     assert name in rust_type_mapping, name
     return rust_type_mapping[name]
 
+def _to_rust_identifier(name):
+    if name == "type":
+        name = "type_"
+    if name[0].isdigit():
+        return 'M' + name
+    return name
+
 # Now the real fun begins
 
 def rs_open(self):
@@ -67,23 +74,19 @@ def rs_open(self):
     _out("use std::error::Error;")
     _out("use crate::xcb_ffi::Connection;")
     _out("use crate::xcb_ffi::SequenceNumber;")
-    _out("struct MyTryError();");
+    _out("use crate::xcb_ffi::Cookie;")
+    _out("pub struct MyTryError();");
 
 def rs_close(self):
     _out_indent_decr()
     _out("}")
 
 def rs_enum(self, name):
-    def name_to_identifier(ename):
-        if ename[0].isdigit():
-            return 'M' + ename
-        return ename
-
     rust_name = _name(name)
-    _out("enum %s {", rust_name)
+    _out("pub enum %s {", rust_name)
     _out_indent_incr()
     for (ename, value) in self.values:
-        _out("%s,", name_to_identifier(ename))
+        _out("%s,", _to_rust_identifier(ename))
     _out_indent_decr()
     _out("}")
 
@@ -104,9 +107,9 @@ def rs_enum(self, name):
     bits = [ename for (ename, bit) in self.bits]
     for (ename, value) in self.values:
         if ename not in bits:
-            _out("%s::%s => %s,", rust_name, name_to_identifier(ename), value)
+            _out("%s::%s => %s,", rust_name, _to_rust_identifier(ename), value)
     for (ename, bit) in self.bits:
-        _out("%s::%s => 1 << %s,", rust_name, name_to_identifier(ename), bit)
+        _out("%s::%s => 1 << %s,", rust_name, _to_rust_identifier(ename), bit)
     _out_indent_decr()
     _out("}")
     _out_indent_decr()
@@ -115,22 +118,22 @@ def rs_enum(self, name):
     _out("}")
 
 def rs_simple(self, name):
-    _out("type %s = %s;", _name(name), _to_rust_type(self.name))
+    _out("pub type %s = %s;", _name(name), _to_rust_type(self.name))
+
+def emit_opcode(name, extra_name, opcode):
+    _out("pub const %s_%s: u8 = %s;", _name(name).upper(), extra_name.upper(), opcode)
 
 def complex_type(self, name, extra_name):
-    opcode = self.opcodes[name]
-    _out("const %s_%s: u8 = %s;", _name(name).upper(), extra_name.upper(), opcode)
-
     is_simple = all(field.type.is_simple or field.type.is_pad for field in self.fields)
     is_fixed_size = all((field.type.fixed_size() and field.type.nmemb == 1) or field.type.is_pad for field in self.fields)
     if (not is_simple) or (not is_fixed_size):
-        print("skipping complicated event", self, name, opcode)
+        print("skipping complicated complex type", self, name)
         return
 
-    _out("struct %s%s {", _name(name), extra_name)
+    _out("pub struct %s%s {", _name(name), extra_name)
     _out_indent_incr()
     for field in self.fields:
-        if not field.type.is_pad:
+        if field.visible:
             _out("pub %s: %s,", field.field_name, _to_rust_type(field.type.name))
     _out_indent_decr()
     _out("}")
@@ -144,7 +147,7 @@ def complex_type(self, name, extra_name):
     for field in self.fields:
         rust_type = _to_rust_type(field.type.name)
         next_offset = offset + field.type.size
-        if not field.type.is_pad:
+        if field.visible:
             _out("let %s = %s::from_ne_bytes(value.get(%s..%s).ok_or(MyTryError())?.try_into().unwrap());",
                     field.field_name, rust_type, offset, next_offset)
         offset = next_offset
@@ -152,7 +155,7 @@ def complex_type(self, name, extra_name):
     _out("Ok(%s%s {", _name(name), extra_name)
     _out_indent_incr()
     for field in self.fields:
-        if not field.type.is_pad:
+        if field.visible:
             _out("%s,", field.field_name)
     _out_indent_decr()
     _out("})")
@@ -168,24 +171,25 @@ def rs_union(self, name):
     print("union", self, name)
 
 def rs_request(self, name):
-    opcode = self.opcode
-    _out("const %s_REQUEST: u8 = %s;", _name(name).upper(), opcode)
+    emit_opcode(name, 'REQUEST', self.opcode)
 
     is_simple = all(field.type.is_simple or field.type.is_pad for field in self.fields)
     is_fixed_size = all((field.type.fixed_size() and field.type.nmemb == 1) or field.type.is_pad for field in self.fields)
     if (not is_simple) or (not is_fixed_size):
-        print("skipping complicated request", self, name, opcode)
-        return
-    if self.reply:
-        print("skipping request with reply", self, name, opcode)
+        print("skipping complicated request", self, name)
         return
 
     args = ["c: &Connection"]
     for field in self.fields:
         if field.visible:
-            args.append("%s: %s" % (field.field_name, _to_rust_type(field.type.name)))
+            args.append("%s: %s" % (_to_rust_identifier(field.field_name), _to_rust_type(field.type.name)))
 
-    _out("fn %s(%s) -> Result<SequenceNumber, Box<dyn Error>> {", _name(name), ", ".join(args))
+    if self.reply:
+        result_type = "Cookie<%sReply>" % _name(name)
+    else:
+        result_type = "SequenceNumber"
+
+    _out("pub fn %s(%s) -> Result<%s, Box<dyn Error>> {", _name(name), ", ".join(args), result_type)
     _out_indent_incr()
 
     request = []
@@ -199,7 +203,7 @@ def rs_request(self, name):
             if field.field_name == "length":
                 value = sum((field.type.size for field in self.fields))
                 _out("let %s: usize = %s / 4;", field.field_name, value)
-            _out("let %s_bytes = %s.to_ne_bytes();", field.field_name, field.field_name)
+            _out("let %s_bytes = %s.to_ne_bytes();", field.field_name, _to_rust_identifier(field.field_name))
             for i in range(field.type.size):
                 request.append("%s_bytes[%d]" % (field.field_name, i))
 
@@ -209,17 +213,31 @@ def rs_request(self, name):
         _out("%s,", byte)
     _out_indent_decr()
     _out("];")
-    _out("c.send_request_without_reply(&[IoSlice::new(&request)])")
+    if self.reply:
+        _out("c.send_request_with_reply(&[IoSlice::new(&request)])")
+    else:
+        _out("c.send_request_without_reply(&[IoSlice::new(&request)])")
     _out_indent_decr()
     _out("}")
+
+    if self.reply:
+        complex_type(self.reply, name, 'Reply')
+        # Work-around for some types not being supported currently and thus not
+        # getting emitted
+        is_simple = all(field.type.is_simple or field.type.is_pad for field in self.reply.fields)
+        is_fixed_size = all((field.type.fixed_size() and field.type.nmemb == 1) or field.type.is_pad for field in self.reply.fields)
+        if not (is_simple and is_fixed_size):
+            _out("pub struct %s%s {}", _name(name), 'Reply')
 
 def rs_eventstruct(self, name):
     print("eventstruct", self, name)
 
 def rs_event(self, name):
+    emit_opcode(name, 'Event', self.opcodes[name])
     complex_type(self, name, 'Event')
 
 def rs_error(self, name):
+    emit_opcode(name, 'Event', self.opcodes[name])
     complex_type(self, name, 'Error')
 
 # We must create an "output" dictionary before any xcbgen imports
