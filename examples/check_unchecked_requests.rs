79a2657ab07f0ea18fe23989ceb01185c34e5861
Uli Schlachter <psychon@znc.in>
2019-11-27 17:11:28 +0100

    Improve docs on X11 error handling
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- examples/check_unchecked_requests.rs
+++ examples/check_unchecked_requests.rs
@@ -1,4 +1,9 @@
 // This program shows error handling. It causes some X11 errors and shows where they end up.
+//
+// This program also serves as a (bad) integration test. To verify that the expected behaviour
+// occurs, it needs to do some extra work. In particular, all calls to .sequence_number() are not
+// needed for the example, but only for the test. Where needed, extra comments indicate how the
+// short version of each case would look.
 
 extern crate x11rb;
 
@@ -28,11 +33,15 @@ fn main() -> Result<(), ConnectionErrorOrX11Error> {
     let seq1 = cookie.sequence_number();
     let res = cookie.reply_unchecked()?;
     assert!(res.is_none());
+    // The short version of the above would be:
+    //   let res = conn.get_geomtry(INVALID_WINDOW)?.reply_unchecked()?;
 
     // Errors can show up as 'events' in wait_for_event() by just dropping the cookie
     let cookie = conn.get_geometry(INVALID_WINDOW)?;
     let seq2 = cookie.sequence_number();
     std::mem::drop(cookie);
+    // The short version of the above would be:
+    //   std::mem::drop(conn.get_geometry(INVALID_WINDOW)?);
 
     // For requests without responses, there are three possibilities
 
@@ -47,12 +56,15 @@ fn main() -> Result<(), ConnectionErrorOrX11Error> {
     let cookie = conn.destroy_window(INVALID_WINDOW)?;
     let seq3 = cookie.sequence_number();
     std::mem::drop(cookie);
+    // The short version of the above would be:
+    //   std::mem::drop(conn.destroy_window(INVALID_WINDOW)?);
 
     // Synchronise with the server so that all errors are already received.
     // FIXME: Add a wrapper to simplify this.
     conn.get_input_focus()?.reply()?;
 
-    // Now check if the things above really caused errors
+    // Now check if the things above really caused errors. This is the part that is supposed to
+    // turn this example into a (bad) integration test.
     for &seq in &[seq1, seq2, seq3] {
         let event = conn.wait_for_event()?;
         assert_eq!(0, event.response_type());
