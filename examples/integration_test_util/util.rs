82df8380748386ddb3454beb86cf9664f225d3e0
Uli Schlachter <psychon@znc.in>
2019-12-04 21:13:49 +0100

    Use LazyAtom in all examples
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- examples/integration_test_util/util.rs
+++ examples/integration_test_util/util.rs
@@ -11,6 +11,7 @@ mod util {
     use x11rb::connection::Connection;
     use x11rb::x11_utils::TryParse;
     use x11rb::generated::xproto::{WINDOW, ConnectionExt as _, EventMask, ClientMessageData, ClientMessageEvent, CLIENT_MESSAGE_EVENT};
+    use x11rb::wrapper::LazyAtom;
 
     pub fn start_timeout_thread<C>(conn: Arc<C>, window: WINDOW)
     where C: Connection + Send + Sync + 'static
@@ -21,26 +22,24 @@ mod util {
             Some(timeout) => timeout
         };
 
-        let (wm_protocols, wm_delete_window) = {
-            let protocols = conn.intern_atom(false, b"WM_PROTOCOLS").unwrap();
-            let delete = conn.intern_atom(false, b"WM_DELETE_WINDOW").unwrap();
-            (protocols.reply().unwrap().atom, delete.reply().unwrap().atom)
-        };
-
-        let mut data = [0; 20];
-        data[..4].copy_from_slice(&wm_delete_window.to_ne_bytes());
-        let (data, _): (ClientMessageData, _) = TryParse::try_parse(&data).unwrap();
-        let event = ClientMessageEvent {
-            response_type: CLIENT_MESSAGE_EVENT,
-            format: 32,
-            sequence: 0,
-            window: window,
-            type_: wm_protocols,
-            data
-        };
-
         thread::spawn(move || {
+            let mut wm_protocols = LazyAtom::new(&*conn, false, b"WM_PROTOCOLS");
+            let mut wm_delete_window = LazyAtom::new(&*conn, false, b"WM_DELETE_WINDOW");
+
             thread::sleep(Duration::from_secs(timeout));
+
+            let mut data = [0; 20];
+            data[..4].copy_from_slice(&wm_delete_window.atom().unwrap().to_ne_bytes());
+            let (data, _): (ClientMessageData, _) = TryParse::try_parse(&data).unwrap();
+            let event = ClientMessageEvent {
+                response_type: CLIENT_MESSAGE_EVENT,
+                format: 32,
+                sequence: 0,
+                window: window,
+                type_: wm_protocols.atom().unwrap(),
+                data
+            };
+
             if let Err(err) = conn.send_event(0, window, EventMask::NoEvent.into(), &event) {
                 eprintln!("Error while sending event: {:?}", err);
             }
