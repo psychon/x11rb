106c13c08f8000b617d06d6023266476652e880b
Uli Schlachter <psychon@znc.in>
2020-01-11 13:45:32 +0100

    Simplify interpreting the 'value' in GetPropertyReply
    The reply to GetProperty contains a 'value' list. In the XML, this is
    described as a list of bytes, i.e. Vec<u8>. However, depending on the
    value of the `format` field of the reply, this actually contains u8,
    u16, or u32. Interpreting this field correctly was complicated.
    
    This commit adds utility methods to `GetPropertyReply`. value8(),
    value16(), and value32() return an iterator that interprets the values
    from the reply as u8, u16, and u32, respectively. This iterator stops on
    the first parsing error and returns None.
    
    The only possible parsing errors here is "too few bytes in input" and it
    should be impossible to hit in well-behaving X11 servers, so I am fine
    with interpreting any errors this way.
    
    Fixes: https://github.com/psychon/x11rb/issues/163
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -1,11 +1,52 @@
 //! Some wrappers around the generated code to simplify use.
 
 use std::convert::TryInto;
+use std::marker::PhantomData;
 
 use super::generated::xproto::{ConnectionExt as XProtoConnectionExt, InternAtomReply, ATOM};
 use super::connection::Connection;
 use super::cookie::{VoidCookie, Cookie};
 use super::errors::{ConnectionError, ConnectionErrorOrX11Error};
+use super::x11_utils::TryParse;
+
+/// Iterator implementation used by `GetPropertyReply`.
+///
+/// This is the actual type returned by `GetPropertyReply::value8`, `GetPropertyReply::value16`,
+/// and `GetPropertyReply::value32`. This type needs to be public due to Rust's visibility rules.
+#[derive(Debug, Clone)]
+pub struct PropertyIterator<'a, T>(&'a [u8], PhantomData<T>);
+
+impl<'a, T> PropertyIterator<'a, T> {
+    pub(crate) fn new(value: &'a [u8]) -> Self {
+        PropertyIterator(value, PhantomData)
+    }
+}
+
+impl<T> Iterator for PropertyIterator<'_, T>
+where T: TryParse
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match T::try_parse(self.0) {
+            Ok((value, remaining)) => {
+                self.0 = remaining;
+                Some(value)
+            },
+            Err(_) => {
+                self.0 = &[];
+                None
+            },
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let size = self.0.len() / std::mem::size_of::<T>();
+        (size, Some(size))
+    }
+}
+
+impl<T: TryParse> std::iter::FusedIterator for PropertyIterator<'_, T> {}
 
 /// Extension trait that simplifies API use
 pub trait ConnectionExt: XProtoConnectionExt {
