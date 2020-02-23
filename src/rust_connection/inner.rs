6523a4c1d5eed4d1c8cef2327e05b2182f13675a
Uli Schlachter <psychon@znc.in>
2020-02-23 10:01:36 +0100

    Fix "dead-lock" in RustConnection with multiple threads
    A single thread that called wait_for_event() would lock the mutex and
    then block while waiting for something to be read. This meant that other
    threads could not write requests any more, because they would have to
    wait for the mutex.
    
    Fix this with a condition variable: A boolean tracks whether some thread
    is currently trying to read a packet from the wire. If there is no
    reader yet, a thread can become the reader and read a packet. While
    reading, it drops the mutex. Another thread that also waits for some
    packet would then see that there is already a reader and then wait on
    the condition variable that signals that something was read.
    
    This commit only handles the "waiting for some packet and still allow
    other threads to sent requests" case. If a thread tries to write to the
    socket, it still blocks out other threads, even if they could make
    progress (e.g. generate_id()). I believe that writing should "almost
    never" block. Let's wait for complaints.
    
    Fixes: https://github.com/psychon/x11rb/issues/203
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- src/rust_connection/inner.rs
+++ src/rust_connection/inner.rs
@@ -36,6 +36,9 @@ where W: Write
     // this struct (for synchronisation reasons).
     write: W,
 
+    // Member that is used by `RustConnection` for synchronisation purpose
+    pub(crate) have_reader: bool,
+
     // The sequence number of the last request that was written
     last_sequence_written: SequenceNumber,
     // Sorted(!) list with information on requests that were written, but no answer received yet.
@@ -61,6 +64,7 @@ where W: Write
         let setup = Self::read_setup(read)?;
         let result = ConnectionInner {
             write,
+            have_reader: false,
             last_sequence_written: 0,
             next_reply_expected: 0,
             last_sequence_read: 0,
