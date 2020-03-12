31595d527b53e5dc241e403624149ee887b9ab1c
Dexter Chua <dalcde@yahoo.com.hk>
2020-03-12 15:08:38 -0400

    Rexport x11rb::generated::FOO as x11rb::FOO

--- src/lib.rs
+++ src/lib.rs
@@ -117,7 +117,8 @@ pub mod wrapper;
 
 use connection::Connection;
 use errors::ConnectError;
-use generated::xproto::{KEYSYM, TIMESTAMP};
+pub use generated::*;
+use xproto::{KEYSYM, TIMESTAMP};
 
 /// Establish a new connection to an X11 server.
 ///
