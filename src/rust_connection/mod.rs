e5e0d060f1c14495084b37c365956c54f12e2860
Uli Schlachter <psychon@znc.in>
2020-05-16 14:08:09 +0200

    Split ConnectionInner::enqueue_packet into two
    FDs and actual packets can trivially be handled separately.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- src/rust_connection/mod.rs
+++ src/rust_connection/mod.rs
@@ -262,7 +262,8 @@ impl RustConnection {
                 drop(lock);
 
                 // 2.5. Actually enqueue the read packet.
-                inner.enqueue_packet(packet, fds);
+                inner.enqueue_fds(fds);
+                inner.enqueue_packet(packet);
 
                 // 2.6. Notify threads that a packet has been enqueued,
                 // so other threads waiting on 1.1 can return.
