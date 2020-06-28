6baa12589c2c2820810aca73829e38ed811f9931
Uli Schlachter <psychon@znc.in>
2020-06-28 13:19:12 +0200

    Add parsing of X11 errors and improve output
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- xtrace-example/src/connection_inner.rs
+++ xtrace-example/src/connection_inner.rs
@@ -1,5 +1,5 @@
 use x11rb::errors::ParseError;
-use x11rb::protocol::{Request, xproto};
+use x11rb::protocol::{Error, Request, xproto};
 use x11rb::x11_utils::{BigRequests, ExtInfoProvider, ExtensionInformation, TryParse, parse_request_header};
 
 fn print_obj_remaining(obj: &impl std::fmt::Debug, data: &[u8], remaining: &[u8]) {
@@ -54,6 +54,7 @@ impl ConnectionInner {
 
     /// Handle the server's Setup (or SetupFailed, SetupAuthenticate)
     pub fn server_setup(&mut self, packet: &[u8]) {
+        print!("server: ");
         match packet[0] {
             0 => print_parse::<xproto::SetupFailed>(packet),
             1 => {
@@ -83,7 +84,7 @@ impl ConnectionInner {
             let request = Request::parse(header, remaining, &mut Vec::new(), &inner.ext_info)?;
             // TODO: Can we get some "remaining" data somehow?
             //print_obj_remaining(&request, packet, remaining);
-            println!("{:?}", request);
+            println!("client: {:?}", request);
             Ok(())
         }
         if let Err(e) = do_parse(self, packet) {
@@ -93,7 +94,14 @@ impl ConnectionInner {
 
     /// Handle an X11 error sent by the server
     pub fn server_error(&mut self, packet: &[u8]) {
-        let _ = packet;
+        fn do_parse(inner: &mut ConnectionInner, packet: &[u8]) -> Result<(), ParseError> {
+            let err = Error::parse(packet, &inner.ext_info)?;
+            println!("server: {:?}", err);
+            Ok(())
+        }
+        if let Err(e) = do_parse(self, packet) {
+            eprintln!("Error while parsing an X11 error: {:?}", e);
+        }
     }
 
     /// Handle an X11 event sent by the server
