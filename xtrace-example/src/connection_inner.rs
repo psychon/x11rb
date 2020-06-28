c46b2027c69077cd79cc7e326d391bf0c79cae52
Uli Schlachter <psychon@znc.in>
2020-06-28 13:51:34 +0200

    Add reply parsing
    The xtrace example can now also parse server replies. This also enables
    managing information about extensions, thus parsing of extension
    requests/errors/events start to work.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- xtrace-example/src/connection_inner.rs
+++ xtrace-example/src/connection_inner.rs
@@ -1,6 +1,9 @@
 use x11rb::errors::ParseError;
-use x11rb::protocol::{Error, Event, Request, xproto};
-use x11rb::x11_utils::{BigRequests, ExtInfoProvider, ExtensionInformation, TryParse, parse_request_header};
+use x11rb::protocol::{Error, Event, Reply, Request, xproto};
+use x11rb::x11_utils::{BigRequests, ExtInfoProvider, ExtensionInformation, TryParse, ReplyParsingFunction, parse_request_header};
+
+use std::collections::VecDeque;
+use std::convert::TryInto;
 
 fn print_obj_remaining(obj: &impl std::fmt::Debug, data: &[u8], remaining: &[u8]) {
     println!("{:?}", obj);
@@ -31,6 +34,8 @@ fn print_parse<T: TryParse + std::fmt::Debug>(data: &[u8]) {
 #[derive(Debug, Default)]
 pub struct ConnectionInner {
     ext_info: ExtInfo,
+    next_client_request: u16,
+    pending_replies: VecDeque<PendingReply>,
 }
 
 impl ConnectionInner {
@@ -50,6 +55,8 @@ impl ConnectionInner {
             );
         }
         print_parse::<xproto::SetupRequest>(packet);
+        assert_eq!(self.next_client_request, 0);
+        self.next_client_request = 1;
     }
 
     /// Handle the server's Setup (or SetupFailed, SetupAuthenticate)
@@ -80,11 +87,36 @@ impl ConnectionInner {
     /// Handle a request sent by the client
     pub fn client_request(&mut self, packet: &[u8]) {
         fn do_parse(inner: &mut ConnectionInner, packet: &[u8]) -> Result<(), ParseError> {
+            let seqno = inner.next_client_request;
+            inner.next_client_request = seqno.wrapping_add(1);
+
             let (header, remaining) = parse_request_header(packet, BigRequests::Enabled)?;
             let request = Request::parse(header, remaining, &mut Vec::new(), &inner.ext_info)?;
             // TODO: Can we get some "remaining" data somehow?
             //print_obj_remaining(&request, packet, remaining);
-            println!("client: {:?}", request);
+            println!("client ({}): {:?}", seqno, request);
+
+            // Is this a QueryExtension?
+            let queried_extension = if let Request::QueryExtension(ref request) = request {
+                match String::from_utf8(request.name.to_vec()) {
+                    Ok(name) => {
+                        println!("Extension name: {}", name);
+                        Some(name)
+                    },
+                    Err(e) => {
+                        println!("Extension name is not utf8: {:?}", e);
+                        None
+                    }
+                }
+            } else {
+                None
+            };
+
+            // Does the request have a reply? If so, remember it.
+            if let Some(parser) = request.reply_parser() {
+                inner.pending_replies.push_back(PendingReply::new(seqno, parser, queried_extension));
+            }
+
             Ok(())
         }
         if let Err(e) = do_parse(self, packet) {
@@ -96,7 +128,13 @@ impl ConnectionInner {
     pub fn server_error(&mut self, packet: &[u8]) {
         fn do_parse(inner: &mut ConnectionInner, packet: &[u8]) -> Result<(), ParseError> {
             let err = Error::parse(packet, &inner.ext_info)?;
-            println!("server: {:?}", err);
+            println!("server ({}): {:?}", err.wire_sequence_number(), err);
+
+            // Remove a pending request if it failed
+            if inner.pending_replies.front().map(|r| r.seqno) == Some(err.wire_sequence_number()) {
+                inner.pending_replies.pop_front();
+            }
+
             Ok(())
         }
         if let Err(e) = do_parse(self, packet) {
@@ -107,8 +145,8 @@ impl ConnectionInner {
     /// Handle an X11 event sent by the server
     pub fn server_event(&mut self, packet: &[u8]) {
         fn do_parse(inner: &mut ConnectionInner, packet: &[u8]) -> Result<(), ParseError> {
-            let err = Event::parse(packet, &inner.ext_info)?;
-            println!("server: {:?}", err);
+            let event = Event::parse(packet, &inner.ext_info)?;
+            println!("server ({}): {:?}", event.wire_sequence_number().unwrap_or(0), event);
             Ok(())
         }
         if let Err(e) = do_parse(self, packet) {
@@ -118,16 +156,75 @@ impl ConnectionInner {
 
     /// Handle a reply sent by the server
     pub fn server_reply(&mut self, packet: &[u8]) {
-        let _ = packet;
+        fn do_parse(inner: &mut ConnectionInner, packet: &[u8]) -> Result<(), ParseError> {
+            let request = match inner.pending_replies.pop_front() {
+                None => {
+                    println!("server: Got unexpected reply {:?}", packet);
+                    return Ok(())
+                },
+                Some(request) => request,
+            };
+
+            let seqno = u16::from_ne_bytes(packet[2..4].try_into().unwrap());
+            if request.seqno != seqno {
+                println!("Expected reply with seqno={}, but got seqno={}", request.seqno, seqno);
+            }
+
+            let (reply, remaining) = (request.parser)(packet, &mut Vec::new())?;
+            print!("server ({}): ", seqno);
+            print_obj_remaining(&reply, packet, remaining);
+
+            if let Some(extension) = request.queried_extension {
+                if let Reply::QueryExtension(reply) = reply {
+                    if reply.present {
+                        let info = ExtensionInformation {
+                            major_opcode: reply.major_opcode,
+                            first_event: reply.first_event,
+                            first_error: reply.first_error,
+                        };
+                        inner.ext_info.add_extension(extension, info);
+                    }
+                }
+            }
+
+            Ok(())
+        }
+        if let Err(e) = do_parse(self, packet) {
+            eprintln!("Error while parsing an X11 event: {:?}", e);
+        }
     }
 }
 
-#[derive(Clone, Default, Debug)]
+struct PendingReply {
+    seqno: u16,
+    parser: ReplyParsingFunction,
+    queried_extension: Option<String>,
+}
+
+impl std::fmt::Debug for PendingReply {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("PendingReply")
+            .field("seqno", &self.seqno)
+            .field("parser", &"<???>")
+            .field("queried_extension", &self.queried_extension)
+            .finish()
+    }
+}
+
+impl PendingReply {
+    fn new(seqno: u16, parser: ReplyParsingFunction, queried_extension: Option<String>) -> Self {
+        Self { seqno, parser, queried_extension }
+    }
+}
+
+/// Information about known extensions
+#[derive(Debug, Default)]
 struct ExtInfo {
     exts: Vec<(String, ExtensionInformation)>,
 }
 
 impl ExtInfo {
+    /// Add a new extension to the state
     fn add_extension(&mut self, name: String, info: ExtensionInformation) {
         self.exts.push((name, info))
     }
