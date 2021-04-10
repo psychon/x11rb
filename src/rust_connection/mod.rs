dbb56eb40db37a41d5a9dbadb4050c2f7468dd64
Uli Schlachter <psychon@znc.in>
2021-04-10 10:26:56 +0200

    Rename ReplyFDKind -> ReplyFdKind
    Clippy says:
    
    warning: name `ReplyFDKind` contains a capitalized acronym
      --> src/rust_connection/mod.rs:48:17
       |
    48 | pub(crate) enum ReplyFDKind {
       |                 ^^^^^^^^^^^ help: consider making the acronym lowercase, except the initial letter: `ReplyFdKind`
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#upper_case_acronyms
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- src/rust_connection/mod.rs
+++ src/rust_connection/mod.rs
@@ -45,7 +45,7 @@ enum MaxRequestBytes {
 type MutexGuardInner<'a> = MutexGuard<'a, inner::ConnectionInner>;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub(crate) enum ReplyFDKind {
+pub(crate) enum ReplyFdKind {
     NoReply,
     ReplyWithoutFDs,
     ReplyWithFDs,
@@ -208,7 +208,7 @@ impl<S: Stream> RustConnection<S> {
         &self,
         bufs: &[IoSlice<'_>],
         fds: Vec<RawFdContainer>,
-        kind: ReplyFDKind,
+        kind: ReplyFdKind,
     ) -> Result<SequenceNumber, ConnectionError> {
         let mut storage = Default::default();
         let bufs = compute_length_field(self, bufs, &mut storage)?;
@@ -251,7 +251,7 @@ impl<S: Stream> RustConnection<S> {
         ];
 
         let seqno = inner
-            .send_request(ReplyFDKind::ReplyWithoutFDs)
+            .send_request(ReplyFdKind::ReplyWithoutFDs)
             .expect("Sending a HasResponse request should not be blocked by syncs");
         inner.discard_reply(seqno, DiscardMode::DiscardReplyAndError);
         let inner = self.write_all_vectored(inner, &[IoSlice::new(&request)], Vec::new())?;
@@ -455,7 +455,7 @@ impl<S: Stream> RequestConnection for RustConnection<S> {
     {
         Ok(Cookie::new(
             self,
-            self.send_request(bufs, fds, ReplyFDKind::ReplyWithoutFDs)?,
+            self.send_request(bufs, fds, ReplyFdKind::ReplyWithoutFDs)?,
         ))
     }
 
@@ -469,7 +469,7 @@ impl<S: Stream> RequestConnection for RustConnection<S> {
     {
         Ok(CookieWithFds::new(
             self,
-            self.send_request(bufs, fds, ReplyFDKind::ReplyWithFDs)?,
+            self.send_request(bufs, fds, ReplyFdKind::ReplyWithFDs)?,
         ))
     }
 
@@ -480,7 +480,7 @@ impl<S: Stream> RequestConnection for RustConnection<S> {
     ) -> Result<VoidCookie<'_, Self>, ConnectionError> {
         Ok(VoidCookie::new(
             self,
-            self.send_request(bufs, fds, ReplyFDKind::NoReply)?,
+            self.send_request(bufs, fds, ReplyFdKind::NoReply)?,
         ))
     }
 
