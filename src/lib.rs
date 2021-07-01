df33dc5ebd98fc396929d052d1dc5c08f588a13b
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2021-07-01 18:39:32 +0200

    Bump MSRV to 1.41
    Allows to use nix 0.21

--- src/lib.rs
+++ src/lib.rs
@@ -137,9 +137,9 @@
     missing_docs,
 )]
 #![allow(
-    // This suggests a method that was stabilised in Rust 1.45, while our MSRV is 1.40.
+    // This suggests a method that was stabilised in Rust 1.45, while our MSRV is 1.41.
     clippy::manual_strip,
-    // This suggests a macro that was stabilised in Rust 1.42, while our MSRV is 1.40.
+    // This suggests a macro that was stabilised in Rust 1.42, while our MSRV is 1.41.
     clippy::match_like_matches_macro,
 )]
 #![cfg_attr(not(feature = "allow-unsafe-code"), forbid(unsafe_code))]
