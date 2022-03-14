00020c7bd9d797287f3f1d19a89374f5c8e4e933
Uli Schlachter <psychon@znc.in>
2022-03-14 09:10:41 +0100

    Refactor: Do not do I/O in IdAllocator
    Previously, the IdAllocator needed an X11 connection and did I/O on this
    connection if it ran out of XIDs. This commit moves that I/O to the
    caller and the IdAllocator just indicates that it ran out of XIDs. A new
    function update_xid_range() is added that gets the resulting
    GetXIDRangeReply and updates the state accordingly.
    
    Moving things around in this way makes it possible to add the
    IdAllocator to the public API since it is reusable. This also allows to
    add a new test case invalid_update_arg() for a case that was previously
    not testable.
    
    Thanks a lot to @notgull for giving me the idea.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- src/rust_connection/id_allocator.rs
+++ src/rust_connection/id_allocator.rs
@@ -1,6 +1,5 @@
-use crate::connection::RequestConnection;
 use crate::errors::{ConnectError, ReplyOrIdError};
-use crate::protocol::xc_misc::{self, ConnectionExt as _, GetXIDRangeReply};
+use crate::protocol::xc_misc::GetXIDRangeReply;
 
 /// An allocator for X11 IDs.
 ///
@@ -36,110 +35,99 @@ impl IdAllocator {
         })
     }
 
-    /// Generate the next ID.
-    ///
-    /// The given connection is used to ask for more IDs if necessary.
-    pub(crate) fn generate_id<C: RequestConnection>(
+    /// Update the available range of IDs based on a GetXIDRangeReply
+    pub(crate) fn update_xid_range(
         &mut self,
-        conn: &C,
-    ) -> Result<u32, ReplyOrIdError> {
-        self.generate_id_impl(|| {
-            if conn
-                .extension_information(xc_misc::X11_EXTENSION_NAME)?
-                .is_none()
-            {
-                // IDs are exhausted and XC-MISC is not available
-                Err(ReplyOrIdError::IdsExhausted)
-            } else {
-                Ok(conn.xc_misc_get_xid_range()?.reply()?)
-            }
-        })
+        xidrange: &GetXIDRangeReply,
+    ) -> Result<(), ReplyOrIdError> {
+        let (start, count) = (xidrange.start_id, xidrange.count);
+        // Apparently (0, 1) is how the server signals "I am out of IDs".
+        // The second case avoids an underflow below and should never happen.
+        if (start, count) == (0, 1) || count == 0 {
+            return Err(ReplyOrIdError::IdsExhausted);
+        }
+        self.next_id = start;
+        self.max_id = start + (count - 1) * self.increment;
+        Ok(())
     }
 
     /// Generate the next ID.
-    ///
-    /// The `get_xid_range` callback is used to request more IDs from the X11 server if necessary.
-    fn generate_id_impl<F>(&mut self, get_xid_range: F) -> Result<u32, ReplyOrIdError>
-    where
-        F: FnOnce() -> Result<GetXIDRangeReply, ReplyOrIdError>,
-    {
+    pub(crate) fn generate_id(&mut self) -> Option<u32> {
         if self.next_id > self.max_id {
-            // Send an XC-MISC GetXIDRange request.
-            let xidrange = get_xid_range()?;
-            let (start, count) = (xidrange.start_id, xidrange.count);
-            // Apparently (0, 1) is how the server signals "I am out of IDs".
-            // The second case avoids an underflow below and should never happen.
-            if (start, count) == (0, 1) || count == 0 {
-                return Err(ReplyOrIdError::IdsExhausted);
-            }
-            self.next_id = start;
-            self.max_id = start + (count - 1) * self.increment;
+            None
+        } else {
+            let id = self.next_id;
+            self.next_id += self.increment;
+            Some(id)
         }
-        assert!(self.next_id <= self.max_id);
-        let id = self.next_id;
-        self.next_id += self.increment;
-        Ok(id)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::errors::ReplyOrIdError;
-    use crate::protocol::xc_misc::GetXIDRangeReply;
-
-    use super::IdAllocator;
-
-    fn unreachable_cb() -> Result<GetXIDRangeReply, ReplyOrIdError> {
-        unreachable!()
-    }
+    use super::{GetXIDRangeReply, IdAllocator, ReplyOrIdError};
 
     #[test]
     fn exhaustive() {
         let mut allocator = IdAllocator::new(0x2800, 0x1ff).unwrap();
         for expected in 0x2800..=0x29ff {
-            assert_eq!(
-                expected,
-                allocator.generate_id_impl(unreachable_cb).unwrap()
-            );
+            assert_eq!(Some(expected), allocator.generate_id());
         }
-        let cb = || -> Result<_, ReplyOrIdError> { Err(ReplyOrIdError::IdsExhausted) };
-        assert!(allocator.generate_id_impl(cb).is_err());
+        assert_eq!(None, allocator.generate_id());
     }
 
     #[test]
     fn increment() {
         let mut allocator = IdAllocator::new(0, 0b1100).unwrap();
-        assert_eq!(0b0000, allocator.generate_id_impl(unreachable_cb).unwrap());
-        assert_eq!(0b0100, allocator.generate_id_impl(unreachable_cb).unwrap());
-        assert_eq!(0b1000, allocator.generate_id_impl(unreachable_cb).unwrap());
-        assert_eq!(0b1100, allocator.generate_id_impl(unreachable_cb).unwrap());
-        let cb = || -> Result<_, ReplyOrIdError> { Err(ReplyOrIdError::IdsExhausted) };
-        assert!(allocator.generate_id_impl(cb).is_err());
+        assert_eq!(Some(0b0000), allocator.generate_id());
+        assert_eq!(Some(0b0100), allocator.generate_id());
+        assert_eq!(Some(0b1000), allocator.generate_id());
+        assert_eq!(Some(0b1100), allocator.generate_id());
+        assert_eq!(None, allocator.generate_id());
     }
 
     #[test]
     fn new_range() {
-        let reply = generate_get_xid_range_reply(0x13370, 3);
         let mut allocator = IdAllocator::new(0x420, 2).unwrap();
-        assert_eq!(0x420, allocator.generate_id_impl(unreachable_cb).unwrap());
-        assert_eq!(0x422, allocator.generate_id_impl(unreachable_cb).unwrap());
-        // At this point the range is exhausted and a GetXIDRange request is sent
-        let cb = || -> Result<_, ReplyOrIdError> { Ok(reply) };
-        assert_eq!(0x13370, allocator.generate_id_impl(cb).unwrap());
-        assert_eq!(0x13372, allocator.generate_id_impl(unreachable_cb).unwrap());
-        assert_eq!(0x13374, allocator.generate_id_impl(unreachable_cb).unwrap());
-        // At this point the range is exhausted and a GetXIDRange request is sent
-        let cb = || -> Result<_, ReplyOrIdError> { Ok(reply) };
-        assert_eq!(0x13370, allocator.generate_id_impl(cb).unwrap());
+        assert_eq!(Some(0x420), allocator.generate_id());
+        assert_eq!(Some(0x422), allocator.generate_id());
+        // At this point the range is exhausted and a GetXIDRange request needs to be sent
+        assert_eq!(None, allocator.generate_id());
+        allocator
+            .update_xid_range(&generate_get_xid_range_reply(0x13370, 3))
+            .unwrap();
+        assert_eq!(Some(0x13370), allocator.generate_id());
+        assert_eq!(Some(0x13372), allocator.generate_id());
+        assert_eq!(Some(0x13374), allocator.generate_id());
+        // At this point the range is exhausted and a GetXIDRange request needs to be sent
+        assert_eq!(None, allocator.generate_id());
+        allocator
+            .update_xid_range(&generate_get_xid_range_reply(0x13370, 3))
+            .unwrap();
+        assert_eq!(Some(0x13370), allocator.generate_id());
     }
 
     #[test]
-    fn invalid_arg() {
+    fn invalid_new_arg() {
         let err = IdAllocator::new(1234, 0).unwrap_err();
         if let super::ConnectError::ZeroIdMask = err {
         } else {
             panic!("Wrong error: {:?}", err);
         }
+    }
+
+    #[test]
+    fn invalid_update_arg() {
+        fn check_ids_exhausted(arg: &Result<(), ReplyOrIdError>) {
+            if let Err(ReplyOrIdError::IdsExhausted) = arg {
+            } else {
+                panic!("Expected IdsExhausted, got {:?}", arg);
+            }
+        }
+
+        let mut allocator = IdAllocator::new(0x420, 2).unwrap();
+        check_ids_exhausted(&allocator.update_xid_range(&generate_get_xid_range_reply(0, 1)));
+        check_ids_exhausted(&allocator.update_xid_range(&generate_get_xid_range_reply(1, 0)));
     }
 
     fn generate_get_xid_range_reply(start_id: u32, count: u32) -> GetXIDRangeReply {
