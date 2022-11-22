396651e0434c8f3af13e5369a4c086b355a17d4c
Uli Schlachter <psychon@znc.in>
2022-11-22 14:07:34 +0100

    Add functions to bitmask_binop
    The bitmask_binop macro is invoked for enumerations that "look like" bit
    masks. This commit adds a contains() associated function that checks if
    "this" mask has all bits set for a given mask.
    
    Additionally, this adds implementations for BitOrAssign and BitAndAssign
    for such an enum with itself. E.g. previously EventMask::NO_EVENT |
    EventMask::KEY_PRESS would not compile.
    
    Finally, this adds some superficial tests for the bitmask_binop
    functions.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

---
+++ x11rb-protocol/tests/enum_tests.rs
@@ -0,0 +1,85 @@
+use x11rb_protocol::protocol::xproto::EventMask;
+
+#[test]
+fn test_conversion() {
+    assert_eq!(0, u32::from(EventMask::NO_EVENT));
+    assert_eq!(1, u32::from(EventMask::KEY_PRESS));
+    assert_eq!(4, u32::from(EventMask::BUTTON_PRESS));
+    assert_eq!(Some(16u32), EventMask::ENTER_WINDOW.into());
+    assert_eq!(EventMask::NO_EVENT, 0u8.into());
+    assert_eq!(EventMask::KEY_PRESS, 1u8.into());
+}
+
+#[test]
+fn test_bit_or() {
+    assert_eq!(
+        EventMask::KEY_PRESS,
+        EventMask::KEY_PRESS | EventMask::NO_EVENT
+    );
+    assert_eq!(EventMask::KEY_PRESS, 1 | EventMask::NO_EVENT);
+    assert_eq!(EventMask::KEY_PRESS, EventMask::NO_EVENT | 1);
+
+    let mut mask = EventMask::KEY_PRESS;
+    mask |= EventMask::BUTTON_PRESS;
+    assert_eq!(5, u32::from(mask));
+
+    let mut mask = EventMask::KEY_PRESS;
+    mask |= 4u32;
+    assert_eq!(5, u32::from(mask));
+
+    let mut mask = 1u32;
+    mask |= EventMask::BUTTON_PRESS;
+    assert_eq!(5, u32::from(mask));
+}
+
+#[test]
+fn test_bit_and() {
+    assert_eq!(
+        EventMask::NO_EVENT,
+        EventMask::KEY_PRESS & EventMask::NO_EVENT
+    );
+    assert_eq!(
+        EventMask::KEY_PRESS,
+        EventMask::KEY_PRESS & EventMask::KEY_PRESS
+    );
+    assert_eq!(
+        EventMask::KEY_PRESS,
+        EventMask::from(5u32) & EventMask::KEY_PRESS
+    );
+    assert_eq!(EventMask::KEY_PRESS, 5 & EventMask::KEY_PRESS);
+    assert_eq!(EventMask::KEY_PRESS, EventMask::KEY_PRESS & 5);
+
+    let mut mask = EventMask::from(5u32);
+    mask &= EventMask::BUTTON_PRESS;
+    assert_eq!(EventMask::BUTTON_PRESS, mask);
+
+    let mut mask = EventMask::from(5u32);
+    mask &= 4u32;
+    assert_eq!(EventMask::BUTTON_PRESS, mask);
+
+    let mut mask = 7u32;
+    mask &= EventMask::from(21u32);
+    assert_eq!(5, u32::from(mask));
+}
+
+#[test]
+fn test_contains() {
+    let mask = EventMask::KEY_PRESS;
+    assert!(mask.contains(EventMask::KEY_PRESS));
+    assert!(mask.contains(EventMask::NO_EVENT));
+    assert!(!mask.contains(EventMask::KEY_PRESS | EventMask::BUTTON_PRESS));
+    assert!(!mask.contains(EventMask::BUTTON_PRESS));
+
+    let mask = EventMask::KEY_PRESS | EventMask::BUTTON_PRESS;
+    assert!(mask.contains(EventMask::KEY_PRESS));
+    assert!(mask.contains(EventMask::BUTTON_PRESS));
+    assert!(mask.contains(EventMask::KEY_PRESS | EventMask::BUTTON_PRESS));
+    assert!(!mask.contains(EventMask::ENTER_WINDOW));
+    assert!(!mask.contains(EventMask::ENTER_WINDOW | EventMask::BUTTON_PRESS));
+
+    assert!(mask.contains(1u32));
+    assert!(mask.contains(4u32));
+    assert!(mask.contains(5u32));
+    assert!(!mask.contains(16u32));
+    assert!(!mask.contains(20u32));
+}
