9ed11e3bb2d50c791d6bb77f40cfb3a9ff173d06
Uli Schlachter <psychon@znc.in>
2023-04-01 17:54:41 +0200

    Add a test for "immediately dropping the driver"
    This commit adds a test case that checks that immediately dropping the
    driver future without polling it once breaks the connection.
    
    Why does this get a special test? Because the usual CallOnDrop
    implementation that constructs some object inside of an "async fn" does
    not work if that future is not even polled once. This special case is
    surprising enough that it deserves a test.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

---
+++ x11rb-async/tests/connection_break_on_drop.rs
@@ -0,0 +1,61 @@
+use futures_lite::future::Ready;
+
+use x11rb::errors::ConnectionError;
+use x11rb::protocol::xproto::Setup;
+use x11rb::rust_connection::{PollMode, Stream};
+use x11rb::utils::RawFdContainer;
+use x11rb_async::connection::Connection;
+use x11rb_async::rust_connection::{RustConnection, StreamBase};
+
+#[derive(Debug)]
+struct FakeStream;
+
+impl Stream for FakeStream {
+    fn poll(&self, _: PollMode) -> Result<(), std::io::Error> {
+        unimplemented!()
+    }
+    fn read(&self, _: &mut [u8], _: &mut Vec<RawFdContainer>) -> Result<usize, std::io::Error> {
+        unimplemented!()
+    }
+    fn write(&self, _: &[u8], _: &mut Vec<RawFdContainer>) -> Result<usize, std::io::Error> {
+        unimplemented!()
+    }
+}
+
+impl StreamBase<'_> for FakeStream {
+    type Readable = Ready<std::io::Result<()>>;
+    type Writable = Ready<std::io::Result<()>>;
+
+    fn readable(&self) -> Self::Readable {
+        unimplemented!()
+    }
+
+    fn writable(&self) -> Self::Writable {
+        unimplemented!()
+    }
+}
+
+#[test]
+fn connection_breaks_on_immediate_drop() {
+    let setup = {
+        let mut setup = Setup::default();
+        setup.resource_id_mask = (1 << 8) - 1;
+        setup
+    };
+    let (conn, driver) = RustConnection::for_connected_stream(FakeStream, setup).unwrap();
+
+    // Drop the driver future. This should break the connection.
+    drop(driver);
+
+    // Check that waiting for input now errors.
+    match async_io::block_on(conn.wait_for_event()) {
+        Err(ConnectionError::IoError(err)) => {
+            assert_eq!(err.kind(), std::io::ErrorKind::Other);
+            assert_eq!(
+                err.into_inner().unwrap().to_string(),
+                "Driving future was dropped"
+            );
+        }
+        _ => unreachable!(),
+    }
+}
