ffa0cf5cf0dbe723f6959693b6b07a9243e9c577
Uli Schlachter <psychon@znc.in>
2023-08-31 17:05:04 +0200

    Add a compile test for Arc in wrapper types
    I noticed that it is relatively easy to add a compile test for the newly
    added feature for the wrapper types of using an Arc instead of a
    reference. Thus, this commit adds such a compile test.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- x11rb/src/test.rs
+++ x11rb/src/test.rs
@@ -1,4 +1,12 @@
-use crate::protocol::xproto::{ModMask, SendEventDest, VisualClass};
+use std::rc::Rc;
+use std::sync::Arc;
+
+use crate::connection::Connection;
+use crate::errors::ReplyOrIdError;
+use crate::protocol::xproto::{
+    ColormapAlloc, ColormapWrapper, FontWrapper, ModMask, SendEventDest, VisualClass, Visualid,
+    Window,
+};
 
 #[test]
 fn test_enum_debug() {
@@ -29,4 +37,18 @@ fn test_bitmask_debug() {
         format!("{:#?}", ModMask::SHIFT | ModMask::LOCK)
     );
     assert_eq!("0", format!("{:?}", ModMask::from(0u8)));
+}
+
+fn _compile_test_arc_in_wrapper<C: Connection>(
+    conn: &Arc<C>,
+) -> Result<FontWrapper<Arc<C>>, ReplyOrIdError> {
+    FontWrapper::open_font(Arc::clone(conn), b"font")
+}
+
+fn _compile_test_rc_in_wrapper<C: Connection>(
+    conn: &Rc<C>,
+    window: Window,
+    visual: Visualid,
+) -> Result<ColormapWrapper<Rc<C>>, ReplyOrIdError> {
+    ColormapWrapper::create_colormap(Rc::clone(conn), ColormapAlloc::NONE, window, visual)
 }
